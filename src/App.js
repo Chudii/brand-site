561b2c4ebbee08518519912d9ff37b704ac14353
Chudi <chudiibida3@gmail.com>
2023-05-16 15:33:58 -0400

    implemented banner

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import './styles/navbar.css'
+import './styles/banner.css'
 import { NavBar } from './components/navbar';
 import { Banner } from './components/banner'
 import 'bootstrap/dist/css/bootstrap.min.css'
